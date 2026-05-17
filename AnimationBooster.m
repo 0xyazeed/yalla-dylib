149d79caac2a4e61de883978fffa821e1b081d9c
0xyazeed <yzeedaloutb@gmail.com>
2026-05-17 11:13:46 +0300

    update dylib

--- AnimationBooster.m
+++ AnimationBooster.m
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <objc/runtime.h>
 
+// تسريع الانميشن
 double BoostAnimation(double duration) {
     return duration * 0.5;
 }
 
-int GetTargetFPS() {
-    return 120;
+// تفعيل/إيقاف السرعة
+static int speedEnabled = 1;
+
+void SetSpeedEnabled(int enabled) {
+    speedEnabled = enabled;
+}
+
+int IsSpeedEnabled() {
+    return speedEnabled;
+}
+
+// تسريع كل شي بالتطبيق
+double GetSpeedMultiplier() {
+    if (speedEnabled) {
+        return 2.0; // ضعف السرعة
+    }
+    return 1.0; // سرعة عادية
 }
